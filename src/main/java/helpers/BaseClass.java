49e5d6e46642343e4eae022f229affae475de88a
madhan007 <mailmadhu19@gmail.com>
2018-08-28 15:29:22 +0100

    commit3

--- src/main/java/helpers/BaseClass.java
+++ src/main/java/helpers/BaseClass.java
@@ -4,15 +4,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 public class BaseClass {
 	
 	
 	protected  WebDriver driver=null;
 	
-	@BeforeMethod
+	@BeforeClass
 	
 	public void startUp() {
 		
@@ -23,7 +23,7 @@ public class BaseClass {
 	
 	}
 	
-	@AfterMethod
+	@AfterClass
 	public void closeBrowser(){
 		
 		driver.quit();
